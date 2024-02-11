088e9213a8e9f208c06454f3560f421a0a999453
AtuL Kumar <atulshahi2003@gmail.com>
2024-02-11 17:28:50 +0530

    completed otp screen

--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
 
 class OtpScreen extends StatefulWidget {
   const OtpScreen({super.key,required this.verificationId});
@@ -15,8 +16,31 @@ class _OtpScreenState extends State<OtpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-         title: Text('otp screens'),
+         title: const Text('Verifying your number'),
+         elevation: 0,
+         backgroundColor: backgroundColor,
+          
        ),
+     body: const Column(
+       crossAxisAlignment: CrossAxisAlignment.center,
+       children: [
+         SizedBox(height: 40,),
+         Text('we have sent an SMS with a code'),
+         Padding(
+           padding: EdgeInsets.symmetric(horizontal: 80.0),
+           child: TextField(
+             decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                )
+             ),
+           ),
+         )
+       ],
+     ), 
     );
   }
 }
